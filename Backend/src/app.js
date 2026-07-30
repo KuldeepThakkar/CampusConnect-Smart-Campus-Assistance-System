65cc11feeae8c7d153aa05d21dd2720cd66ae532
krishpatel20217 <krishpatel20217@gmail.com>
2026-07-30 21:46:39 +0530

    Complete academic selection unit 2.1

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const campusRoutes = require("./routes/campus.routes");
 const navigationRoutes = require("./routes/navigation.routes");
 const timetableRoutes = require("./routes/timetable.routes");
 
-app.use(express.json());
 
+app.use(express.json());
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+    })
+);
 
 app.use("/api/campus", campusRoutes);
 app.use("/api/navigation", navigationRoutes);
--- Frontend/src/pages/Selection.jsx
+++ Frontend/src/pages/Selection.jsx
@@ -1,9 +1,53 @@
+import { useEffect, useState } from "react";
+import api from "../api/axios";
+
 function Selection(){
 
+    const [academicData, setAcademicData] = useState({});
+    const [department, setDepartment] = useState("");
+
+    const departments = Object.keys(academicData);
+
+    useEffect(() => {
+        
+        fetchAcademicOptions()
+        
+        
+    }, []);
+
+    const fetchAcademicOptions = async () => {
+        try {
+            const response = await api.get("/timetable/options");
+
+            setAcademicData(response.data.data);
+
+        } catch (error) {
+            console.error(error);
+        }
+    };    
+
+    
+    console.log(academicData);
     return(
-        <div>
-            Academic Selection
-        </div>
+    <div>
+        <h2>Academic Selection</h2>
+        <label htmlFor="department">
+            Department
+        </label>
+
+        <select
+            value={department}
+            onChange={(e) => setDepartment(e.target.value)}
+        >
+            <option value="">Select Department</option>
+
+            {departments.map((dept) => (
+                <option key={dept} value={dept}>
+                    {dept}
+                </option>
+            ))}
+        </select>
+    </div>
     )
 
 }
