c583bf3c2b44f83087a34f211ae3f55aed5de160
krishpatel20217 <krishpatel20217@gmail.com>
2026-09-01 17:02:00 +0530

    feat(frontend): add classroom service functions for free/schedule lookups

---
+++ Frontend/src/services/classroom.js
@@ -0,0 +1,30 @@
+import api from "../api/axios";
+import { getCampusData } from "./campus";
+
+export const getFreeClassroomsNow = async ({ day, time, building }) => {
+
+    const response = await api.get("/classrooms/free", {
+        params: { day, time, building }
+    });
+
+    return response.data;
+
+};
+
+export const getDaySchedule = async ({ day, building }) => {
+
+    const response = await api.get("/classrooms/schedule", {
+        params: { day, building }
+    });
+
+    return response.data;
+
+};
+
+export const getBuildingsWithClassrooms = async () => {
+
+    const response = await getCampusData();
+
+    return response.data.buildings.filter((b) => b.classrooms && b.classrooms.length > 0);
+
+};
